5013aca680d05e4ef726d3ea9fe623812933ae80
giedrys-michal <giedrys.michal@gmail.com>
2020-12-20 23:24:07 +0100

    Add 5 tests to new ApteczkaService functionality
    Added new 5 tests

--- ApteczkaMaven/src/test/java/serviceTests/ApteczkaServiceTests.java
+++ ApteczkaMaven/src/test/java/serviceTests/ApteczkaServiceTests.java
@@ -17,6 +17,39 @@ public class ApteczkaServiceTests {
 	Lek lek_1 = new Lek(0, 0, "Lek_1", "Tabletka", 20, 45.0);
 	Lek lek_2 = new Lek(1, 0, "Lek_2", "Tabletka", 40, 15.0);
 	
-	
 	Apteczka ap_1 = new Apteczka(0, "Apteczka_1", DateTime.now(), ls.leki);
+	
+	@Test
+	public void isApteczka_1NazwaEqual() {
+		as.addApteczka(ap_1);
+		assertEquals("Apteczka_1", as.getApteczka(0).getNazwa());		
+	}
+	
+	@Test
+	public void isApteczka_1LekiEmpty() {
+		as.addApteczka(ap_1);
+		assertTrue(as.getApteczka(0).leki.isEmpty());
+	}
+	
+	@Test
+	public void isApteczka_1LekiNotEmpty() {
+		ls.addLek(lek_1);
+		as.addApteczka(ap_1);
+		assertFalse(as.getApteczka(0).leki.isEmpty());
+	}
+	
+	@Test
+	public void wasLekAddedToApteczka_1() {
+		as.addApteczka(ap_1);
+		int countInit = as.getApteczka(0).getLeki().size();
+		ls.addLek(lek_1);
+		int countModif = as.getApteczka(0).getLeki().size();
+		assertEquals(++countInit, countModif);
+	}
+	
+	@Test
+	public void isApteczka_1DataUtworzeniaNotNull() {
+		as.addApteczka(ap_1);
+		assertNotNull(as.getApteczka(0).getData_utworzenia());
+	}
 }
